484bc3a66aab8dfde93832185fdb01e55ffd6d0c
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-01-26 00:17:15 +0300

    Add continue with second practice

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+/**
+ * `cargo check` controls whether have error without generating binary files
+ * `cargo build` builds the project
+ * `cargo run` builds & run the project
+ * `rustfmt filename.rs` formats the file
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
